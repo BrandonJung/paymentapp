098a70f54092b3123f7b5eef71524ef6ead791a6
BrandonJung <Brandonjung111@gmail.com>
2025-03-19 04:34:56 -0700

    Clean slate 2.0

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   );
 }
