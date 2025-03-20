2124b64eac609741cbf6a30597635156e8852e86
BrandonJung <Brandonjung111@gmail.com>
2025-03-20 06:24:47 -0700

    Dummy data + added prime icons

---
+++ dummyData.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    label: 'Home',
+    icon: 'pi pi-home',
+  },
+  {
+    label: 'Features',
+    icon: 'pi pi-star',
+  },
+  {
+    label: 'Our Partners',
+    icon: 'pi pi-search',
+  },
+  {
+    label: 'Contact',
+    icon: 'pi pi-envelope',
+  },
+];
