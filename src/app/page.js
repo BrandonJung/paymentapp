88d38d62f66462c9d033df20767e061b658d437c
BrandonJung <Brandonjung111@gmail.com>
2025-03-20 06:25:23 -0700

    MenuBar + Login Modal

--- src/app/page.js
+++ src/app/page.js
@@ -1,58 +1,98 @@
-import Image from 'next/image';
+'use client';
+
 import { Menubar } from 'primereact/menubar';
-// import styles from "./page.module.css";
+import { items } from '../../dummyData';
+import { Button } from 'primereact/button';
+import { useState } from 'react';
+import { Dialog } from 'primereact/dialog';
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
 
-const items = [
-  {
-    label: 'Home',
-    icon: 'pi pi-home',
-  },
-  {
-    label: 'Features',
-    icon: 'pi pi-star',
-  },
-  {
-    label: 'Projects',
-    icon: 'pi pi-search',
-    items: [
-      {
-        label: 'Components',
-        icon: 'pi pi-bolt',
-      },
-      {
-        label: 'Blocks',
-        icon: 'pi pi-server',
-      },
-      {
-        label: 'UI Kit',
-        icon: 'pi pi-pencil',
-      },
-      {
-        label: 'Templates',
-        icon: 'pi pi-palette',
-        items: [
-          {
-            label: 'Apollo',
-            icon: 'pi pi-palette',
-          },
-          {
-            label: 'Ultima',
-            icon: 'pi pi-palette',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: 'Contact',
-    icon: 'pi pi-envelope',
-  },
-];
+const fieldWidth = '25vw';
+const fieldMargin = 8;
 
-export default function Home() {
-  return (
+const Home = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginButton = (
     <div>
-      <Menubar model={items} />
+      <Button
+        label='Log In'
+        onClick={() => {
+          setModalVisible(true);
+        }}
+      />
     </div>
   );
-}
+
+  return (
+    <div>
+      <Menubar model={items} end={loginButton} />
+      <Dialog
+        header=''
+        showHeader={true}
+        visible={modalVisible}
+        draggable={false}
+        dismissableMask={true}
+        style={{
+          width: '35vw',
+          height: '30vw',
+        }}
+        contentStyle={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        onHide={() => {
+          if (!modalVisible) return;
+          setModalVisible(false);
+        }}>
+        <div>
+          <label style={{ margin: fieldMargin }}>Welcome Back!</label>
+          <FloatLabel
+            style={{ margin: fieldMargin, marginBottom: 22, marginTop: 14 }}>
+            <InputText
+              style={{ width: fieldWidth }}
+              id='username'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor='username'>Username</label>
+          </FloatLabel>
+          <FloatLabel style={{ margin: fieldMargin, marginBottom: 14 }}>
+            <Password
+              inputStyle={{ width: fieldWidth }}
+              inputId='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              feedback={false}
+            />
+            <label htmlFor='password'>Password</label>
+          </FloatLabel>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <label style={{ margin: fieldMargin, fontSize: 14 }}>
+              Forgot Password?
+            </label>
+            <label style={{ margin: fieldMargin, fontSize: 14 }}>
+              New? Sign Up!
+            </label>
+          </div>
+          <Button
+            label='Log In'
+            style={{
+              margin: fieldMargin,
+              width: fieldWidth,
+              marginTop: 14,
+              marginBottom: 66,
+            }}
+          />
+        </div>
+      </Dialog>
+    </div>
+  );
+};
+
+export default Home;
