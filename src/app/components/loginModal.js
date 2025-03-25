9e18bdb82f99cb07adbaddf567c44744b4e6741e
BrandonJung <Brandonjung111@gmail.com>
2025-03-25 15:15:50 -0700

    LoginModal Component

---
+++ src/app/components/loginModal.js
@@ -0,0 +1,81 @@
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import { FloatLabel } from 'primereact/floatlabel';
+import { InputText } from 'primereact/inputtext';
+import { Password } from 'primereact/password';
+
+const fieldWidth = '25vw';
+const fieldMargin = 8;
+
+const LoginModal = ({
+  modalVisible,
+  setModalVisible,
+  username,
+  setUsername,
+  password,
+  setPassword,
+}) => {
+  return (
+    <Dialog
+      header=''
+      showHeader={true}
+      visible={modalVisible}
+      draggable={false}
+      dismissableMask={true}
+      style={{
+        width: '35vw',
+        height: '30vw',
+      }}
+      contentStyle={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      onHide={() => {
+        if (!modalVisible) return;
+        setModalVisible(false);
+      }}>
+      <div>
+        <label style={{ margin: fieldMargin }}>Welcome Back!</label>
+        <FloatLabel
+          style={{ margin: fieldMargin, marginBottom: 22, marginTop: 14 }}>
+          <InputText
+            style={{ width: fieldWidth }}
+            id='username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor='username'>Username</label>
+        </FloatLabel>
+        <FloatLabel style={{ margin: fieldMargin, marginBottom: 14 }}>
+          <Password
+            inputStyle={{ width: fieldWidth }}
+            inputId='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            feedback={false}
+          />
+          <label htmlFor='password'>Password</label>
+        </FloatLabel>
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <label style={{ margin: fieldMargin, fontSize: 14 }}>
+            Forgot Password?
+          </label>
+          <label style={{ margin: fieldMargin, fontSize: 14 }}>
+            New? Sign Up!
+          </label>
+        </div>
+        <Button
+          label='Log In'
+          style={{
+            margin: fieldMargin,
+            width: fieldWidth,
+            marginTop: 14,
+            marginBottom: 66,
+          }}
+        />
+      </div>
+    </Dialog>
+  );
+};
+export default LoginModal;
--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,7 @@ import { Menubar } from 'primereact/menubar';
 import { items } from '../../dummyData';
 import { Button } from 'primereact/button';
 import { useState } from 'react';
-import { Dialog } from 'primereact/dialog';
-import { FloatLabel } from 'primereact/floatlabel';
-import { InputText } from 'primereact/inputtext';
-import { Password } from 'primereact/password';
-
-const fieldWidth = '25vw';
-const fieldMargin = 8;
+import LoginModal from './components/loginModal';
 
 const Home = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -31,66 +25,14 @@ const Home = () => {
   return (
     <div style={{ maxWidth: 1550, width: '100%' }}>
       <Menubar model={items} end={loginButton} />
-      <Dialog
-        header=''
-        showHeader={true}
-        visible={modalVisible}
-        draggable={false}
-        dismissableMask={true}
-        style={{
-          width: '35vw',
-          height: '30vw',
-        }}
-        contentStyle={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-        onHide={() => {
-          if (!modalVisible) return;
-          setModalVisible(false);
-        }}>
-        <div>
-          <label style={{ margin: fieldMargin }}>Welcome Back!</label>
-          <FloatLabel
-            style={{ margin: fieldMargin, marginBottom: 22, marginTop: 14 }}>
-            <InputText
-              style={{ width: fieldWidth }}
-              id='username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <label htmlFor='username'>Username</label>
-          </FloatLabel>
-          <FloatLabel style={{ margin: fieldMargin, marginBottom: 14 }}>
-            <Password
-              inputStyle={{ width: fieldWidth }}
-              inputId='password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              feedback={false}
-            />
-            <label htmlFor='password'>Password</label>
-          </FloatLabel>
-          <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <label style={{ margin: fieldMargin, fontSize: 14 }}>
-              Forgot Password?
-            </label>
-            <label style={{ margin: fieldMargin, fontSize: 14 }}>
-              New? Sign Up!
-            </label>
-          </div>
-          <Button
-            label='Log In'
-            style={{
-              margin: fieldMargin,
-              width: fieldWidth,
-              marginTop: 14,
-              marginBottom: 66,
-            }}
-          />
-        </div>
-      </Dialog>
+      <LoginModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
     </div>
   );
 };
